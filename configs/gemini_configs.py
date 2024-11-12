9080c2009464c3502495ba956cbf1bc5fcc929e3
Chansung Park <deep.diver.csp@gmail.com>
2024-11-12 09:36:23 +0900

    add podcast specific configs

--- configs/gemini_configs.py
+++ configs/gemini_configs.py
@@ -230,3 +230,35 @@ extract_section_details_config = {
         "response_mime_type": "application/json",
     }
 }
+
+write_script_config = {
+    "model_name": "gemini-1.5-flash-002",
+    "generation_config": {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 40,
+        "max_output_tokens": 8192,
+        "response_schema": content.Schema(
+            type=content.Type.OBJECT,
+            required=["conversations"],
+            properties={
+                "conversations": content.Schema(
+                    type=content.Type.ARRAY,
+                    items=content.Schema(
+                        type=content.Type.OBJECT,
+                        required=["Alex", "Jamie"],
+                        properties={
+                            "Alex": content.Schema(
+                                type=content.Type.STRING,
+                            ),
+                            "Jamie": content.Schema(
+                                type=content.Type.STRING,
+                            ),
+                        },
+                    ),
+                ),
+            },
+        ),
+        "response_mime_type": "application/json",
+    }
+}
